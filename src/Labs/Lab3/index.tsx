81a52b9cf24388937426b1053f38731b79541fe9
ShireenKumar <shireen.kumar1304@gmail.com>
2025-02-18 03:34:27 -0500

    Finished 3.1-3.2

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -1,7 +1,30 @@
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariablesTypes from "./VariableTypes";
+import BooleanVariables from "./BooleanVariables";
+import IfElse from "./IfElse";
+import TernaryOperator from "./TernaryOperator";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import LegacyFunctions from "./LegacyFunctions";
+import ArrowFunctions from "./ArrowFunctions";
+import ImpliedReturn from "./ImpliedReturn";
+import TemplateLiterals from "./TemplateLiterals";
+
 export default function Lab3() {
-    return (
-      <div>
-        <h2>Lab 3</h2>
-      </div>
-  );}
-  
+  return(
+    <div id="wd-lab3">
+      <h3>Lab 3</h3>
+      <VariablesAndConstants/>
+      <VariablesTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputInline/>
+      <ConditionalOutputIfElse/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturn />
+      <TemplateLiterals />
+    </div>
+  );
+}
