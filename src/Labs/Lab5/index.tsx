427c62d5c2a3dcec89f60fce29fd29fbf34d0041
ShireenKumar <shireen.kumar1304@gmail.com>
2025-03-27 05:19:53 -0400

    Stopped at 5.2.5.2

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,6 +1,7 @@
 import EnvironmentVariables from "./EnvironmentVariables";
 import PathParameters from "./PathParameters";
 import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithArrays from "./WorkingWithArrays";
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 export default function Lab5() {
   return (
@@ -14,6 +15,7 @@ export default function Lab5() {
       <EnvironmentVariables />
       <PathParameters />
       <WorkingWithObjects />
+      <WorkingWithArrays />
     </div>
   );
 }
