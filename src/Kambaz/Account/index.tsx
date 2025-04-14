468b809b704adbe97bc5e7e08df0ca66ac69de97
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-14 02:40:08 -0400

    6.2.6.2

--- src/Kambaz/Account/index.tsx
+++ src/Kambaz/Account/index.tsx
@@ -4,6 +4,7 @@ import Signin from "./Signin";
 import Signup from "./Signup";
 import Profile from "./Profile";
 import AccountNavigation from "./Navigation";
+import Users from "./Users";
 
 export default function Account() {
   const { currentUser } = useSelector((state: any) => state.accountReducer);
@@ -25,6 +26,7 @@ export default function Account() {
                 <Route path="/Signin" element={<Signin />} />
                 <Route path="/Signup" element={<Signup />} />
                 <Route path="/Profile" element={<Profile />} />
+                <Route path="/Users" element={<Users />} />
               </Routes>
             </td>
           </tr>
