d8efb5072f632f9f6bc7420e7cf28cb380d11f83
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-13 21:59:38 -0400

    deployment

--- src/Labs/Lab5/HttpClient.tsx
+++ src/Labs/Lab5/HttpClient.tsx
@@ -1,19 +1,14 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { useState, useEffect } from "react";
+
+
 import * as client from "./client";
-
-const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
-
 export default function HttpClient() {
   const [welcomeOnClick, setWelcomeOnClick] = useState("");
   const [welcomeOnLoad, setWelcomeOnLoad] = useState("");
-
+  
   const fetchWelcomeOnClick = async () => {
-    const response = await axios.get(`${REMOTE_SERVER}/lab5/welcome`);
     const message = await client.fetchWelcomeMessage();
     setWelcomeOnClick(message);
-
-    setWelcomeOnClick(response.data);
   };
   const fetchWelcomeOnLoad = async () => {
     const welcome = await client.fetchWelcomeMessage();
@@ -30,10 +25,10 @@ export default function HttpClient() {
       <button className="btn btn-primary me-2" onClick={fetchWelcomeOnClick}>
         Fetch Welcome
       </button> <br />
+      Response from server: <b>{welcomeOnClick}</b>
       <h4>Requesting on Load</h4>
       Response from server: <b>{welcomeOnLoad}</b>
       <hr />
-
     </div>
   );
 }
