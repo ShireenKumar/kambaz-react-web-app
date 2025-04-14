585828eb6471aa9b156fc4668cb1184ce161a8c6
ShireenKumar <shireen.kumar1304@gmail.com>
2025-04-14 17:50:16 -0400

    finished until 6.3

--- src/Kambaz/Courses/People/Table.tsx
+++ src/Kambaz/Courses/People/Table.tsx
@@ -1,6 +1,7 @@
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
+import PeopleDetails from "./Details";
 
 export default function PeopleTable({
   users = [],
@@ -23,6 +24,7 @@ export default function PeopleTable({
 
   return (
     <div id="wd-people-table">
+      <PeopleDetails />
       <Table striped>
         <thead>
           <tr>
@@ -37,17 +39,20 @@ export default function PeopleTable({
         <tbody>
           {enrolledUsers.map((user) => (
             <tr key={user._id}>
-              <td className="wd-full-name text-nowrap">
-                <FaUserCircle className="me-2 fs-1 text-secondary" />
-                <span className="wd-first-name">{user.firstName} </span>
-                <span className="wd-last-name">{user.lastName}</span>
-              </td>
-              <td className="wd-login-id">{user.loginId}</td>
-              <td className="wd-section">{user.section}</td>
-              <td className="wd-role">{user.role}</td>
-              <td className="wd-last-activity">{user.lastActivity}</td>
-              <td className="wd-total-activity">{user.totalActivity}</td>
-            </tr>
+            <td className="wd-full-name text-nowrap">
+              <Link to={`/Kambaz/Account/Users/${user._id}`} className="text-decoration-none">
+
+          <FaUserCircle className="me-2 fs-1 text-secondary" />
+          <span className="wd-first-name">{user.firstName} </span>
+          <span className="wd-last-name">{user.lastName}</span>
+          </Link>
+        </td>
+        <td className="wd-login-id border">{user.loginId}</td>
+        <td className="wd-section border">{user.section}</td>
+        <td className="wd-role border">{user.role}</td>
+        <td className="wd-last-activity border">{user.lastActivity}</td>
+        <td className="wd-total-activity border">{user.totalActivity}</td>
+      </tr>
           ))}
         </tbody>
       </Table>
